d1620724fb49d207bd5514a1cea69ef60e65a815
Nagamasa <nagamasa70@yahoo.co.jp>
2019-08-06 21:56:14 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,98 @@
+let timer = document.getElementById('timer');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+let reset = document.getElementById('reset');
+let start = document.getElementById('start');
+let startTime;
+let countDownTime = 0;
+let timeLeft;
+let timerId;
+let isStart = false;
+
+function updateTimer(time) {
+    let date = new Date(time);
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    let milliSecond = date.getMilliseconds();
+    let timerString;
+    // 数字0埋め
+    minute = ('0' + minute).slice(-2);
+    second = ('0' + second).slice(-2);
+    milliSecond = ('00' + milliSecond).slice(-3);
+    timerString = `${minute}:${second}.${milliSecond}`;
+    // timerに反映
+    timer.textContent = timerString;
+    document.title = timerString;
+}
+
+function countDown() {
+    timerId = setTimeout(function() {
+        timeLeft = countDownTime - (Date.now() - startTime);
+
+        if (timeLeft < 0) {
+            isStart = false;
+            start.textContent = 'Start';
+            // タイマーストップ
+            clearTimeout(timerId);
+            // 残り時間を0にする
+            timeLeft = 0;
+            // カウントダウン終了後再度カウントダウンしないようにする
+            countDownTime = 0;
+            updateTimer(timeLeft);
+
+            alert('Time up!');
+            return;
+        }
+        updateTimer(timeLeft);
+        countDown();
+    }, 10)
+}
+// event
+// startボタン
+start.addEventListener('click', function() {
+    if (isStart === false) {
+        isStart = true;
+        start.textContent = 'Stop';
+        startTime = Date.now();
+        countDown();
+    } else {
+        isStart = false;
+        start.textContent = 'Start';
+        countDownTime = timeLeft;
+        clearTimeout(timerId);
+    }
+
+});
+// minボタン
+min.addEventListener('click', function() {
+    if (isStart === true) {
+        return;
+    }
+    // 1分ずつ加算
+    countDownTime += 60 * 1000;
+    // 60分を超えたら0にする
+    if (countDownTime >= 60 * 60 * 1000) {
+        countDownTime = 0;
+    }
+    updateTimer(countDownTime);
+
+});
+// secボタン
+sec.addEventListener('click', function() {
+    if (isStart === true) {
+        return;
+    }
+    // 1秒ずつ加算
+    countDownTime += 1000;
+    // 60分を超えたら0にする
+    if (countDownTime >= 60 * 60 * 1000) {
+        countDownTime = 0;
+    }
+    updateTimer(countDownTime);
+});
+// resetボタン
+reset.addEventListener('click', function() {
+    // 1秒ずつ加算
+    countDownTime = 0;
+    updateTimer(countDownTime);
+});
